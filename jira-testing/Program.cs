394db99485eab4f4d5e0d0ee1ec5c0c8484443d5
Michael Hurwitz <amh77054@gmail.com>
2024-09-01 10:26:07 +0300

    first commit

---
+++ jira-testing/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace jira_testing
+{
+    internal class Program
+    {
+        static void BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+
+            BubbleSort(array);
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
